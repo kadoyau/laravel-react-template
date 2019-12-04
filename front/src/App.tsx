08b8ee6b75f45af42ca430c5b466409da2e482ad
kadoyau <el.psi.congroo@gmail.com>
2019-12-05 03:41:42 +0900

    DBに入ったユーザ名を表示する

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {gql} from 'apollo-boost';
+import {Query} from 'react-apollo';
+
+const GET_USERS = gql`
+    query {
+        users {
+            data {
+                name
+            }
+        }
+    }
+`;
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <Query query={GET_USERS}>
+                    {({loading, error, data}: any) => {
+                        if (loading) return <div>Loading...</div>;
+                        if (error) return <div>Error :(</div>;
+                        return (
+                            <div>
+                                {data.users.data.map((user: any) => <p>{user.name}</p>)}
+                            </div>
+                        )
+                    }}
+                </Query>
+            </header>
+        </div>
+    );
+};
 
 export default App;
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Pass your GraphQL endpoint to uri
+const client = new ApolloClient({uri: '/graphql'});
+
+const ApolloApp = (AppComponent: React.FC) => (
+    <ApolloProvider client={client}>
+        <AppComponent/>
+    </ApolloProvider>
+);
+
+ReactDOM.render(ApolloApp(App), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
